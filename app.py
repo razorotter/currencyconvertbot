7514f6dfac479e6f0446d43dbb9ce8b36dbbf020
razorotter <120640237+razorotter@users.noreply.github.com>
2023-01-24 16:39:26 +0700

    1

---
+++ app.py
@@ -0,0 +1,32 @@
+import requests
+import telebot
+import json
+
+from config import keys, TOKEN
+from extensions import APIException, Converter
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать, введите команду в формате:\n<валюта> \
+<в какую валюту перевод> \
+<переводимое количество>\nСписок доступных валют: /values'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    quote, base, amount = message.text.split(' ')
+    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}')
+    total_base = json.loads(r.content)[keys[base]]
+    text = f'Цена {amount} {quote} в {base} - {(float(total_base) * float(amount))}'
+    bot.send_message(message.chat.id, text)
+
+bot.polling(none_stop=True)
